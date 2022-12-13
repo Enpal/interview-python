6acf06c7cc22e0b55d5c484102336a3a48e5aafc
Andreas Grever <agrever@mailbox.org>
2022-12-13 14:02:22 +0100

    support redis and rabbitmq

--- tasks.py
+++ tasks.py
@@ -1,8 +1,17 @@
 from celery import Celery
 from main import prepare_forecast as prepare_forecast_sync, get_weather_forecast as get_weather_forecast_sync
 
-BROKER_URL = 'redis://localhost:6379/0'
-BACKEND_URL = 'redis://localhost:6379/1'
+BROKER_AND_BACKEND = "rabbitmq"
+
+if BROKER_AND_BACKEND == "rabbitmq":
+    BROKER_URL = 'amqp://guest:guest@localhost:5672'
+    BACKEND_URL = 'rpc://guest:guest@localhost:5672'
+
+else:
+    # redis
+    BROKER_URL = 'redis://localhost:6379/0'
+    BACKEND_URL = 'redis://localhost:6379/1'
+
 app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
 
 @app.task
